5fff6639518a20447589a265b786b9b27bb2a552
easildur24 <lixiaoqiang@lixiaoqangsimac.attlocal.net>
2015-08-31 22:25:38 -0700

    remove

--- api/constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DEV_KEY', 'b7190b84-484d-4cc7-88ca-8e2b90fb7f56');
-define('GET_NA_CHAMPION_1_2', 'https://na.api.pvp.net/api/lol/na/v1.2/champion?api_key=' . DEV_KEY);
-define('GET_NA_STATIC_CHAMPION_1_2', 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=all&api_key=' .DEV_KEY);
-define('GET_NA_STATIC_MAP_DETAILS_1_2', 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/map?api_key=' . DEV_KEY);
-define('GET_NA_STATIC_MASTERY_1_2', 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/mastery?masteryListData=all&api_key=' . DEV_KEY);
-?>
--- database/constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOSTNAME', 'localhost');
-define('DB_NAME', 'lolelf');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-?>
